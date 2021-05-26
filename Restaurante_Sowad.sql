a633e986a42503f2f4160e6e22b7e0af19d1d42d
Miguel Quiroz <N00127923@upn.pe>
2021-05-26 18:55:41 -0500

    SQL Ver 2

--- Restaurante_Sowad.sql
+++ Restaurante_Sowad.sql
@@ -1,17 +1,23 @@
+/*Creacion de la base de datos*/
 create database restaurante_sowad;
 
+/*Uso de base de datos creada*/
 use restaurante_sowad;
+
+/*Creacion de tablas*/
 
 create table Tbl_Productos (
 idProductos varchar(100) primary key not null,
 Nombre varchar(100),
 Precio double,
-idTipoCategoria int
+idTipoCategoria int,
+estado varchar(50)
 );
 
 create table Tbl_CategoriaProductos (
 idCategoria int primary key not null AUTO_INCREMENT,
-Descripcion varchar(100)
+Descripcion varchar(100),
+estado bit not null
 );
 
 create table Tbl_IngredientesXProducto (
@@ -29,7 +35,8 @@ idMarca int
 
 create table Tbl_Marca (
 idMarca int primary key not null AUTO_INCREMENT ,
-Descripcion varchar(100)
+Descripcion varchar(100),
+estado bit not null
 );
 
 
@@ -42,7 +49,8 @@ nacimiento date,
 telefono varchar(50),
 correo varchar(100),
 idCargo int,
-idUsuario int
+idUsuario int,
+estado varchar(50)
 );
 
 create table Tbl_HorarioEmpleado(
@@ -54,14 +62,16 @@ hora_salida datetime
 
 create table Tbl_Cargo (
 idCargo int primary key not null AUTO_INCREMENT,
-Descripcion varchar(50)
+Descripcion varchar(50),
+estado bit not null
 );
 
 create table Tbl_Usuario (
 idUsuario int primary key not null AUTO_INCREMENT,
 Usuario varchar(100),
 Contraseña varchar(100),
-TipoUsuario Varchar(10) /*Si es de clientes o empleados*/
+TipoUsuario Varchar(10), /*Si es de clientes o empleados*/
+estado varchar(50)
 );
 
 
@@ -90,55 +100,72 @@ idCliente varchar(100) primary key not null,
 Nombres varchar(50),
 Apellido varchar(50),
 Telefono varchar(10),
-idUsuario int
+idUsuario int,
+estado varchar(50)
 );
 
 create table Tbl_Combos (
      idProducto varchar(100) not null,
      idProductosContenido varchar(100) not null,
      Primary key (idProducto,idProductosContenido),
-     constraint foreign key idProd (idProducto) references  Tbl_Productos(idProductos),
-     constraint foreign key idProdCont (idProductosContenido) references Tbl_Productos(idProductos)
+     constraint foreign key idProd (idProducto) references  Tbl_Productos(idProductos)
+     on delete cascade on update cascade,
+     constraint foreign key idProdCont (idProductosContenido) references Tbl_Productos(idProductos) 
+     on delete cascade on update cascade
 );
 
+/*Relaciones de tablas*/
+
 ALTER TABLE Tbl_Pedidos /*PEDIDO - CLIENTES*/
-ADD FOREIGN KEY (idCliente) REFERENCES Tbl_Cliente(idCliente);
+ADD FOREIGN KEY (idCliente) REFERENCES Tbl_Cliente(idCliente) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Productos /*PRODUCTOS - CATEGORIA*/
-ADD FOREIGN KEY (idTipoCategoria) REFERENCES Tbl_CategoriaProductos(idCategoria);
+ADD FOREIGN KEY (idTipoCategoria) REFERENCES Tbl_CategoriaProductos(idCategoria) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_IngredientesXProducto /*INGREDIENTEXPRODUCTO - PRODUCTO*/
-ADD FOREIGN KEY (idProducto) REFERENCES Tbl_Productos (idProductos);
+ADD FOREIGN KEY (idProducto) REFERENCES Tbl_Productos (idProductos) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_IngredientesXProducto /*INGREDIENTESXPRODUCTO - INGREDIENTES*/
-ADD FOREIGN KEY (idIngredientes) REFERENCES Tbl_Ingredientes(idIngredientes) ;
+ADD FOREIGN KEY (idIngredientes) REFERENCES Tbl_Ingredientes(idIngredientes) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Ingredientes /*INGREDIENTES-MARCA*/
-ADD FOREIGN KEY (idMarca) REFERENCES Tbl_Marca(idMarca); 
+ADD FOREIGN KEY (idMarca) REFERENCES Tbl_Marca(idMarca) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Detalle_Pedidos  /*DETALLE PEDIDO - PRODUCTOS */
-ADD FOREIGN KEY (idProducto) REFERENCES Tbl_Productos (idProductos);
+ADD FOREIGN KEY (idProducto) REFERENCES Tbl_Productos (idProductos) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Detalle_Pedidos  /* DETALLE_PEDIDO PEDIDO*/
-ADD FOREIGN KEY (idPedido) REFERENCES Tbl_Pedidos (idPedidos);
+ADD FOREIGN KEY (idPedido) REFERENCES Tbl_Pedidos (idPedidos) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Pedidos /*PEDIDO - EMPLEADO*/
-ADD FOREIGN KEY (idEmpleado) REFERENCES Tbl_Empleado (idEmpleado);
+ADD FOREIGN KEY (idEmpleado) REFERENCES Tbl_Empleado (idEmpleado) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Empleado /*EMPLEADO - CARGO*/
-ADD FOREIGN KEY (idCargo) REFERENCES Tbl_Cargo (idCargo);
+ADD FOREIGN KEY (idCargo) REFERENCES Tbl_Cargo (idCargo) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_HorarioEmpleado /*HORARIO EMPLEADO - EMPLEADO*/
-ADD FOREIGN KEY (idEmpleado) REFERENCES Tbl_Empleado(idEmpleado);
+ADD FOREIGN KEY (idEmpleado) REFERENCES Tbl_Empleado(idEmpleado) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Cliente /*CLIENTE - USUARIOO*/
-ADD FOREIGN KEY (idUsuario) REFERENCES Tbl_Usuario (idUsuario);
+ADD FOREIGN KEY (idUsuario) REFERENCES Tbl_Usuario (idUsuario) 
+on update cascade on delete cascade;
 
 ALTER TABLE Tbl_Empleado /*EMPLEADO - USUARIO */
-ADD FOREIGN KEY (idUsuario) REFERENCES Tbl_Usuario (idUsuario);
+ADD FOREIGN KEY (idUsuario) REFERENCES Tbl_Usuario (idUsuario) 
+on update cascade on delete cascade;
 
 
-/*Iniciar Sesion*/
+/*-------------------------Iniciar Sesion-------------------------*/
 DELIMITER @@ 
 create procedure InicioSesion(
 Usu varchar(100),
@@ -148,7 +175,7 @@ out Verdad bit
 sp:Begin
     Declare exiteUsuario int;
     set exiteUsuario = (Select count(*) from Tbl_Usuario where Usuario = Usu and Contraseña = Con);
-    if exiteUsuario = 0 THEN
+    if exiteUsuario < 1 THEN
        SET Verdad = 0;
        leave sp;
 	else
@@ -158,47 +185,130 @@ sp:Begin
 End @@
 DELIMITER ;
 
-/*Registrar Cliente*/
+
+
+
+/*-------------------------Triggers-------------------------*/
+
+/*Codigo Cliente*/
 DELIMITER @@
-CREATE PROCEDURE RegistrarCliente(
+create trigger Cliente_Codigo
+BEFORE insert on tbl_Cliente for each row
+begin
+declare Siguiente_Codigo int;
+set Siguiente_Codigo = (select ifnull(max(Convert(substring(idCliente,8),signed integer)),0) from tbl_Cliente) + 1;
+   set new.idcliente = concat('CLI',lpad(siguiente_Codigo,8,'0'));
+END @@
+DELIMITER ;
+
+/*Codigo Empleado*/
+DELIMITER @@
+CREATE TRIGGER Empleado_Codigo
+BEFORE INSERT on tbl_empleado FOR each row
+begin
+declare Siguiente_Codigo int;
+set siguiente_Codigo = (select ifnull(max(Convert(substring(idEmpleado,8),signed integer)),0) from tbl_empleado) + 1; 
+set new.idempleado = concat('EMP',lpad(siguiente_Codigo,8,'0'));
+end @@
+DELIMITER ;
+
+
+/*PROCEDIMIENTOS ALMACENADOS*/
+
+/*-------------------------Cliente-------------------------*/
+
+
+/*Mostrar*/
+DELIMITER @@
+CREATE PROCEDURE cliente_Mostrar()
+Begin
+Select cli.idCliente,cli.Nombres,cli.Apellido,cli.Telefono,usu.Usuario,usu.contraseña
+From tbl_Cliente cli 
+inner join tbl_Usuario usu on cli.idUsuario = usu.idUsuario;
+end @@
+DELIMITER ;
+
+/*Registrar */
+DELIMITER @@
+CREATE PROCEDURE Cliente_Registrar(
 Nom Varchar(50),
 Ape varchar(50),
 Tel varchar(50),
 Usu varchar(100),
-Con varchar(100)
+Con varchar(100),
+out mensaje varchar(100)
 )
 sp:Begin
-    Declare IdUs,numeroFilas,NumeroUsuario int;
-    declare Codigo,Mensaje varchar(50);
-    set Codigo = (select Concat('CLI',Right(Concat('00000000',convert(((max(substring(idCliente,4,8))*1)+1),varchar(10))),8)) as Codigo from Tbl_Cliente);
-    set numeroFilas = (select count(*) from Tbl_Cliente);
+    Declare IdUs,NumeroUsuario int;
     set Mensaje = "Usuario Ya Existe. Por Favor, Eliga Otro Usuario";
     set NumeroUsuario = (select count(*) from Tbl_Usuario where usuario = usu and contraseña = Con and TipoUsuario = "CLI" Limit 1);
-    if numeroFilas = 0  then  
-               Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario) values(Usu,Con,"CLI");
-               set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "CLI" Limit 1);
-               Insert Into Tbl_Cliente(idCliente,Nombres,Apellido,Telefono,idUsuario) values ("CLI00000001",Nom,Ape,Tel,idUs);
-    elseif numeroFilas > 0 and NumeroUsuario = 0 then 
-			   Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario) values(Usu,Con,"CLI");
-			   set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "CLI" Limit 1);
-			   Insert Into Tbl_Cliente(idCliente,Nombres,Apellido,Telefono,idUsuario) values (Codigo,Nom,Ape,Tel,idUs);
-	else 
-        select Mensaje;
-        leave sp;
-    end if;
+    if NumeroUsuario > 0 then 
+      select Mensaje;
+      leave sp;
+    else 
+	Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario,Estado) values(Usu,Con,"CLI","ACTIVO");
+	set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "CLI" Limit 1);
+	Insert Into Tbl_Cliente(Nombres,Apellido,Telefono,idUsuario,Estado) values (Nom,Ape,Tel,idUs,"ACTIVO");
+end if;
 End @@
 DELIMITER ;
 
-/*Registrar Cargo*/
+
+/*Actualizar*/
 DELIMITER @@
-CREATE PROCEDURE RegistrarCargo(
+CREATE PROCEDURE Cliente_Actualizar(
+Nom Varchar(50),
+Ape varchar(50),
+Tel varchar(50),
+Usu varchar(100),
+Con varchar(100),
+idCli VARCHAR(100),
+out mensaje varchar(100)
+)
+BEGIN 
+declare codigo_usu int;
+set codigo_usu = (Select idUsuario from Tbl_Cliente where idCliente = idCLi);
+update tbl_usuario set Usuario = Usu, Contraseña = Con where idUsuario = codigo_usu;
+update tbl_Cliente set nombres = Nom, Apellido = Ape, Telefono = Tel where idCliente = idCli;
+END @@
+DELIMITER ;
+
+
+/*Eliminar*/
+DELIMITER @@
+CREATE PROCEDURE Cliente_Eliminar(
+    idCli varchar(50)
+)
+begin
+declare codigo_usu int;
+set codigo_usu = (Select idUsuario from Tbl_cliente where idCliente = idCli);
+update tbl_usuario set Estado = "Eliminado" where idUsuario = codigo_usu;
+update tbl_cliente set Estado = "Eliminado" where idCliente = idCli;
+end @@
+DELIMITER ;
+
+
+
+/*-------------------------Cargo-------------------------*/
+
+/*Mostrar*/
+DELIMITER @@
+create PROCEDURE cargo_Mostrar()
+begin
+select idCargo,Descripcion from Tbl_Cargo;
+end @@
+DELIMITER ;
+
+/*Registrar*/
+DELIMITER @@
+CREATE PROCEDURE Cargo_Registrar(
 Descrip varchar(100),
 out valor bit
 )
 sp:Begin
 Declare Filas int;
 set Filas = (Select count(*) From Tbl_Cargo where Descripcion = Descrip );
-if Filas = 0 then 
+if Filas < 1 then 
     insert into Tbl_Cargo(Descripcion) value(Descrip);
     set valor = 1;
     leave sp;
@@ -209,7 +319,24 @@ end if;
 End @@
 DELIMITER ;
 
-/*Registrar Empleado*/
+
+/*Eliminar*/
+DELIMITER @@
+Create PROCEDURE Cargo_Eliminar(
+idCar int
+)
+Begin
+update Tbl_Cargo set Estado = 0 where IdCargo = idCar;
+end @@
+DELIMITER ;
+
+
+/*-------------------------Empleado-------------------------*/
+
+/*Mostrar*/
+Select * from Tbl_Empleado ;
+
+/*Registrar*/
 DELIMITER @@
 CREATE PROCEDURE RegistrarEmpleado(
 Nom Varchar(50),
@@ -220,48 +347,36 @@ tel varchar(50),
 cor varchar(100),
 idCar int,
 usu varchar(100),
-Con varchar(100)
+Con varchar(100),
+out Mensaje varchar(100)
 )
 sp:Begin
-    Declare IdUs,numeroFilas,NumeroUsuario int;
-    declare Codigo,Mensaje varchar(50);
-    set Codigo = (select Concat('EMP',Right(Concat('00000000',convert(((max(substring(idEmpleado,4,8))*1)+1),varchar(10))),8)) as Codigo from Tbl_Empleado);
-    set numeroFilas = (select count(*) from Tbl_Empleado);
+    Declare IdUs,NumeroUsuario int;
     set Mensaje = "Usuario Ya Existe. Por Favor, Eliga Otro Usuario";
     set NumeroUsuario = (select count(*) from Tbl_Usuario where usuario = usu and contraseña = Con and TipoUsuario = "EMP" Limit 1);
-    if numeroFilas = 0  then  
-               Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario) values(Usu,Con,"EMP");
-               set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "EMP" Limit 1);
-               Insert Into Tbl_Empleado(idEmpleado,nombres,apellidos,dni,nacimiento,telefono,correo,idCargo,idUsuario) values ("EMP00000001",Nom,Ape,dni,Nac,Tel,cor,idCar,idUs);
-    elseif numeroFilas > 0 and NumeroUsuario = 0 then 
-			   Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario) values(Usu,Con,"EMP");
-			   set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "EMP" Limit 1);
-			   Insert Into Tbl_Empleado(idEmpleado,nombres,apellidos,dni,nacimiento,telefono,correo,idCargo,idUsuario) values (Codigo,Nom,Ape,dni,Nac,Tel,cor,idCar,idUs);
-	else 
-        select Mensaje;
-        leave sp;
-    end if;
+    if NumeroUsuario > 0 then
+    select Mensaje;
+    leave sp;
+    else
+	Insert Into Tbl_Usuario(Usuario,Contraseña,TipoUsuario,Estado) values(Usu,Con,"EMP","ACTIVO");
+	set idUs = (Select idUsuario from  Tbl_Usuario where Usuario = Usu and Contraseña=Con and TipoUsuario = "EMP" Limit 1);
+	Insert Into Tbl_Empleado(nombres,apellidos,dni,nacimiento,telefono,correo,idCargo,idUsuario,Estado) values (Nom,Ape,dni,Nac,Tel,cor,idCar,idUs,"ACTIVO");
+end if;
 End @@
 DELIMITER ;
 
+/*Pruebas*/
+CALL Cliente_Registrar("Yaneli Elvita","Carpio Arevalo","970271929","ADMIN4","12345",@mensaje);
+
+call InicioSesion("ADMIN4","12345",@vedad);
+
+delete from tbl_usuario where idUsuario = 1;
+
+select * from tbl_Cliente;
+
+select * from tbl_usuario;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-CALL RegistrarCliente("Yaneli Elvita","Carpio Arevalo","970271929","ADMIN4","12345",@mensaje);
-
-
-select * from Tbl_Cliente;
 
 
 /*Reinicar El auto_Increment de la tabla*/
